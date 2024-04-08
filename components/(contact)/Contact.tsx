485b0a2dd6bb86c411958ff3d24d6a63aa3cdefd
zephrous <danielpoh3131@gmail.com>
2024-04-08 16:56:33 +0800

    update contact us

--- components/(contact)/Contact.tsx
+++ components/(contact)/Contact.tsx
@@ -107,7 +107,7 @@ export const ContactPage: React.FC = () => {
             </div>
             <button
                 type='submit'
-                className='bg-sky-900 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
+                className='bg-sky-950 hover:bg-sky-850 text-white font-bold py-2 px-5 justify-center text-center items-center rounded focus:outline-none focus:shadow-outline'
             >
                 Send Email
             </button>
